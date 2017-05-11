b619ebde948e5bc62447f3a1d7810193265e7684
wemmm <jenny.wem@gmail.com>
2017-05-11 15:11:02 +0100

    Removed magic numbers and added constant for station capacity.
    - Added constant DEFAULT_CAPACITY to dockingstation.rb
    - Updated dockingstation.rb and dockingstation_spec.rb to use DEFAULT_CAPACITY rather than magic numbers.

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -38,7 +38,7 @@ describe DockingStation do
     end
 
     it 'raises an error when attempting to dock a bike in a full station' do
-      20.times { subject.dock_bike(Bike.new) }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(Bike.new) }
       expect { subject.dock_bike Bike.new }.to raise_error 'Docking station full'
     end
 
