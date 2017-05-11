362bcd6965aed827799eb00bf8e093696568d48d
bannastre <bannastre@gmail.com>
2017-05-11 09:45:08 +0100

    refactored dockingstation_spec.rb

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,44 +1,45 @@
 require "dockingstation.rb"
 
 describe DockingStation do
+
+  it { is_expected.to respond_to(:dock_bike).with(1).argument}
+  it { is_expected.to respond_to :bike}
+
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
       subject.dock_bike(bike)
       expect(subject.release_bike).to eq bike
     end
+
+    it "releases working bikes" do
+      #bike = subject.release_bike
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(bike).to be_working
+    end
+
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
-end
 
   describe '#dock_bike' do
     it 'raises an error when attempting to dock a bike in a full station' do
       subject.dock_bike(Bike.new)
       expect { subject.dock_bike Bike.new }.to raise_error 'Docking station full'
     end
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.bike).to eq bike
+    end
   end
-
-  it "releases working bikes" do
-    #bike = subject.release_bike
-    bike = Bike.new
-    expect(bike).to be_working
-  end
-
-  it { is_expected.to respond_to(:dock_bike).with(1).argument}
-
-  it { is_expected.to respond_to :bike}
-
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock_bike(bike)).to eq bike
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock_bike(bike)
-    expect(subject.bike).to eq bike
-  end
-
 
 end
